312a94b3245542433c735f09167b4a32033317b5
Miguel Ramos <return.infox@gmail.com>
2022-07-30 12:35:04 -0400

    traeremos todos los productos

--- src/products/views/ListProductsView.vue
+++ src/products/views/ListProductsView.vue
@@ -1,10 +1,28 @@
 <template>
   <p>Esta es la Lista de los productos</p>
+  <pre>{{ productos }}</pre>
 </template>
 
 <script>
+// *http://localhost:8080/products/list-products
+import { mapActions, mapState } from 'vuex';
+import CardProducto from '../components/CardProducto.vue';
 export default {
-
+  name: 'HomeView',
+  components: {
+    CardProducto
+  },
+  methods: {
+    ...mapActions({
+      products: 'getProductsApi'
+    })
+  },
+  computed: {
+    ...mapState(['productos'])
+  },
+  created() {
+    this.products();
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,40 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    productos: [],
+    productoObject: {},
+    usuario: null,
   },
   mutations: {
+    setProductos(state, payload) {
+      state.productos = payload;
+    },
+    setProductById(state, payload) {
+      state.productoObject = payload;
+    }
   },
   actions: {
+    async getProductsApi({commit}) {
+      try {
+        const response = await fetch('http://localhost:3000/products');
+        const data = await response.json();
+        console.log(data); //* El arreglo de los productos 8 elementos
+        commit('setProductos', data);
+      } catch (error) {
+        throw error;
+      }
+    },
+   async getProductByIdApi({commit}, id) {
+    try {
+      const response = await fetch(`http://localhost:3000/products/${id}`);
+      const data = await response.json();
+      commit('setProductById', data);
+    } catch (error) {
+      throw error;
+    }
+   }
+  },
+  getters: {
   },
   modules: {
   }
